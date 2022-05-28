f660ab9b87dfd4e329adb160cc8cbe56541ee03b
yaseenit7 <71213860+yaseenit7@users.noreply.github.com>
2022-05-28 16:29:54 +0530

    Update networksweep.sh

--- networksweep.sh
+++ networksweep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nmap -sn $1 -oG hosts.txt 2>/dev/null;cat hosts.txt | cut -d " " -f 2 | grep -iv nmap | tee livehosts.txt;rm hosts.txt
+nmap -sn $1 -oG hosts.txt 1>/dev/null;cat hosts.txt | cut -d " " -f 2 | grep -iv nmap | tee livehosts.txt;rm hosts.txt
